0042f93866b4c90ce48b6844bb601ad0f476c4f9
Michal Korman <michal.korman@icloud.com>
2023-11-25 16:01:18 +0100

    helper functions for config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,14 @@
 import 'dotenv/config';
 import {z} from 'zod';
 
+const str = () => z.string();
+const bool = () => z.string().transform(v => ['true', '1'].includes(v.toLowerCase()));
+const int = () => z.preprocess(Number, z.number().int());
+
 const ConfigSchema = z.object({
-  DISCORD_APP_ID: z.string(),
-  DISCORD_TOKEN: z.string(),
-  IGNORED_MODULES: z.string().optional().transform(
+  DISCORD_APP_ID: str(),
+  DISCORD_TOKEN: str(),
+  IGNORED_MODULES: str().optional().transform(
     v => new Set<string>(v ? v.split(',') : [])
   )
 });
